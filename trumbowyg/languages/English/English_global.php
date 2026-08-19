2b2846501b1999398a348d557cedef3059c8557a
Alex <info@exas.nl>
2026-08-19 11:32:59 +0200

    Update English_global.php

--- trumbowyg/languages/English/English_global.php
+++ trumbowyg/languages/English/English_global.php
@@ -5,6 +5,8 @@
  * Language file for "trumbowyg" plugin.
  */
 
-define("LAN_PLUGIN_TRUMBOWYG_NAME", "TrumboWyg");
-define("LAN_PLUGIN_TRUMBOWYG_DESC", "e107 integration for TrumboWyg - A lightweight WYSIWYG editor");
-define("LAN_PLUGIN_TRUMBOWYG_SUMM", "Light, translatable and customisable jQuery plugin Beautiful design, generates semantic code, comes with a powerful API");
+return [
+    'LAN_PLUGIN_TRUMBOWYG_NAME' => "TrumboWyg",
+    'LAN_PLUGIN_TRUMBOWYG_DESC' => "e107 integration for TrumboWyg - A lightweight WYSIWYG editor",
+    'LAN_PLUGIN_TRUMBOWYG_SUMM' => "Light, translatable and customisable jQuery plugin Beautiful design, generates semantic code, comes with a powerful API",
+];
